522861fb2d2ce2a7ab0f346db54b2e2509b9b442
Artem Dorofeev <a.dorofeev@ukr.net>
2023-09-06 11:45:10 +0300

    add thread

--- main_hw06_my.py
+++ main_hw06_my.py
@@ -1,11 +1,18 @@
 import sys
 from pathlib import Path
+import os
 # import uuid
 import shutil
-import time
+from time import time
 from threading import Thread
+import logging
 
 from main_hw06_normalize import normalize
+
+logger = logging.getLogger()
+stream_handler = logging.StreamHandler()
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
 
 
 CATEGORIES = {"Audio": [".mp3", ".aiff", ".m3u"],
@@ -44,16 +51,13 @@ def get_categories(file: Path) -> str:
     return "Other"
 
 
-def sort_folder(path: Path) -> None:
+def sort_folder(path: Path, destination_path: Path) -> None:
     global count_files
-    global LIST_FOLDERS_SORT
-    objects_list = path.glob("**/*")
-    for item in [i for i in objects_list if i.name not in LIST_FOLDERS_SORT]:
-
+    for item in path.glob("**/*"):
         if item.is_file():
             count_files += 1
-            cat = get_categories(item)
-            move_file(item, path, cat)
+            category = get_categories(item)
+            move_file(item, destination_path, category)
 
 
 def del_emppty_folders(path: Path) -> None:
@@ -91,6 +95,10 @@ def main():
     global count_del_folder
     global count_unpack_arch
 
+    st_time = time()
+
+    logging.debug("\nProgramm Started")
+
     try:
         path = Path(sys.argv[1])
     except IndexError:
@@ -98,16 +106,41 @@ def main():
 
     if not path.exists():
         return f"Folder with path {path} dos`n exists."
+    
+    items = os.listdir(path)
 
-    sort_folder(path)
-    print(f'Sorted - {count_files} files')
+    threads = []
+    i = 1
+    for item in items:
+        item_path = os.path.join(path, item)
+        item_path = Path(item_path)
+        if os.path.isdir(item_path):
+            thread = Thread(
+                name=f"thread0{i}", target=sort_folder, args=(item_path, path)
+            )
+            logging.debug(f"{thread.name} Started")
+            i += 1
+            threads.append(thread)
+            thread.start()
+
+        else:
+            category = get_categories(item_path)
+            move_file(item_path, path, category)
+
+    for thread in threads:
+        thread.join()
+
     del_emppty_folders(path)
     print(f'Deleted - {count_del_folder} folder')
     upack_archive(path)
     print(f'Unpack - {count_unpack_arch} archive and delete files')
 
-    return "All ok"
+    end_time = time()
+    delta_time = end_time - st_time
+
+    return f"All ok, time work - {delta_time}"
 
 
 if __name__ == "__main__":
     print(main())
+
